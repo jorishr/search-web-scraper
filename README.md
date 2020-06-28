2d2022201282a186eb6618a74f66b25ded5b9361
jorishr <joris@jorisr.com>
2020-06-28 17:03:20 +0200

    npm script updated; search type argument added

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ NodeJs app that scrapes the Google Search results page.
 Strategy pattern approach: 
 - main or generic search functionality
 - specifically tweaked search code for searching documents and videos
+
+## Search
+npm start `query` `type`
+
+Type is optional and expected to be video or document.
+Type defaults to all.
--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 const search = require('./search');
 
 (async () => {
-    const query = process.argv.slice(2);
+    //const query = process.argv.slice(2);
+    const query = process.argv[2];
+    const type  = process.argv[3] || 'all';
+    if(!query) return console.log('No search query parameter!')
     console.log('####################');
     console.log(`Query: ${query}`);
     console.log('####################');
-    const results = await search(query, 'all');
+    const results = await search(query, type);
     console.log(results);
     console.log('####################');
 })();
